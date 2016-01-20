6f8a9bb0f7a18da3b61faf7630bc6fb07ef90eb9
chiwingsit <chi-wing.sit@mail.mcgill.ca>
2016-01-19 23:24:37 -0500

    Fix contact info padding

--- js/init.js
+++ js/init.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     $('.scrollspy').scrollSpy();
 });
+(function($){
+  $(function(){
+
+    // Floating-Fixed table of contents
+    if ($('nav').length) {
+      $('.toc-wrapper').pushpin({ top: $('nav').height() });
+    }
+    else if ($('#index-banner').length) {
+      $('.toc-wrapper').pushpin({ top: $('#index-banner').height() });
+    }
+    else {
+      $('.toc-wrapper').pushpin({ top: 0 });
+    }
+        }); // end of document ready
+})(jQuery); // end of jQuery name space
