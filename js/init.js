b6cc6e46bd2c9b381ed85e658f5b01ea4fbc898b
chiwingsit <chi-wing.sit@mail.mcgill.ca>
2016-02-14 12:08:24 -0500

    Scrollfire for lists

--- js/init.js
+++ js/init.js
@@ -4,6 +4,17 @@ $(document).ready(function(){
     $('.modal-trigger').leanModal();
 });
 console.log("Hello World");
+var options = [
+    {selector: '#proficient-skills', offset: 0, callback: 'Materialize.showStaggeredList("#proficient-skills")' },
+    {selector: '#inter-skills', offset: 0, callback: 'Materialize.showStaggeredList("#inter-skills")' },
+    {selector: '#novice-skills', offset: 0, callback: 'Materialize.showStaggeredList("#novice-skills")' },
+    {selector: '#list-ubisoft', offset: 0, callback: 'Materialize.showStaggeredList("#list-ubisoft")' },
+    {selector: '#list-appvelopers', offset: 0, callback: 'Materialize.showStaggeredList("#list-appvelopers")' },
+    {selector: '#list-silanis', offset: 0, callback: 'Materialize.showStaggeredList("#list-silanis")' },
+    {selector: '#list-codejam', offset: 0, callback: 'Materialize.showStaggeredList("#list-codejam")' },
+    {selector: '#list-radioip', offset: 0, callback: 'Materialize.showStaggeredList("#list-radioip")' }
+];
+Materialize.scrollFire(options);
 (function($){
   $(function(){
 
